ddfe0f4dc7cbff3f89298cb2765c0e917f403395
ayush <rakholiyaayush861@gmail.com>
2025-11-28 14:19:28 -0500

    Add checksum calculation and validation logic based on ALT, PITCH, and BANK fields

--- Fdms.Shared/ChecksumCalculator.cs
+++ Fdms.Shared/ChecksumCalculator.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using System;
+
+namespace Fdms.Shared
+{
+
+    public static class ChecksumCalculator
+    {
+
+        public static int CalculateChecksum(TelemetryRecord record)
+        {
+            if (record == null)
+            {
+                throw new ArgumentNullException(nameof(record));
+            }
+
+            return CalculateChecksum(record.Altitude, record.Pitch, record.Bank);
+        }
+
+
+        public static int CalculateChecksum(double altitude, double pitch, double bank)
+        {
+            double average = (altitude + pitch + bank) / 3.0;
+
+            int checksum = (int)Math.Round(average, MidpointRounding.AwayFromZero);
+
+            return checksum;
+        }
+    }
+}
