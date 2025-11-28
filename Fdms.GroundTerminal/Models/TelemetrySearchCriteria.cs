6070bb68870300139b54b46634ceececd3921cb2
Jal Shah <jalshah381@gmail.com>
2025-11-28 17:37:57 -0500

    implemented search criteria

--- Fdms.GroundTerminal/Models/TelemetrySearchCriteria.cs
+++ Fdms.GroundTerminal/Models/TelemetrySearchCriteria.cs
@@ -1,1 +1,27 @@
-﻿
+﻿/* 
+ *  FILE          : TelemetrySearchCriteria.cs
+ *  PROJECT       :  FDMS (Flight Data Management System)
+ *  PROGRAMMER    : Ayushkumar Rakholiya, Jal Shah, Viraj Solanki and Darsh Patel
+ *  FIRST VERSION : 2025-11-20
+ *  DESCRIPTION   :
+    *    This file defines the TelemetrySearchCriteria class which encapsulates
+    *    the search criteria for querying telemetry data.
+ */
+using System;
+using System.Data.SqlClient;
+
+
+namespace Fdms.GroundTerminal.Models
+{
+    //-------------------------------------------------------------
+    // CLASS : TelemetrySearchCriteria
+    // PURPOSE :
+    //   Encapsulates the search criteria for querying telemetry data.
+    //-------------------------------------------------------------
+    public sealed class TelemetrySearchCriteria
+    {
+        public string? TailNumber { get; set; }
+        public DateTime? FromTimestamp { get; set; }
+        public DateTime? ToTimestamp { get; set; }
+    }
+}
